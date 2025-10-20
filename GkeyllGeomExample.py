c04132ee1e7f60a02941ef9cbfa39442071e5822
akashukla <akashukla@utexas.edu>
2025-10-20 10:15:33 -0500

    Add the final example script used to generate a good mapping

--- GkeyllGeomExample.py
+++ GkeyllGeomExample.py
@@ -184,39 +184,34 @@ inp = [inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11]
 
 #If not loading from file do this:
 
-# gefit = gkeyllEFIT.gkeyllEFIT('./test_data/gkeyll_data/', 'step')
-# gefit.load_eqdsk()
-# gefit.find_xpts()
-# # Can replace find_xpts with the line below to save time
-# #gefit.Rxpt = np.array([2.489562969077551, 2.489562969077551])
-# #gefit.Zxpt = np.array([-6.18070007806902,  6.18070007806902])
-# #gefit.psisep=1.5092
-# gg = gkeyllGeom.gkeyllGeom(efit = gefit, gridspec = inp)
+gefit = gkeyllEFIT.gkeyllEFIT('./test_data/gkeyll_data/', 'step')
+gefit.load_eqdsk()
+gefit.find_xpts()
+# Can replace find_xpts with the line below to save time
+#gefit.Rxpt = np.array([2.489562969077551, 2.489562969077551])
+#gefit.Zxpt = np.array([-6.18070007806902,  6.18070007806902])
+#gefit.psisep=1.5092
+gg = gkeyllGeom.gkeyllGeom(efit = gefit, gridspec = inp)
+gg.write()
 
 #If loading from file we can do this:
-gg = gkeyllGeom.gkeyllGeom('./gkeyllGeometry/stored_data/gkeyllGeometry.pkl')
+#gg = gkeyllGeom.gkeyllGeom('./gkeyllGeometry/stored_data/gkeyllGeometry.pkl')
 
 
 
-# Tests 
-# Call  it on a point in the inner SOL
-#gg.psitheta(1.59,0.0)
+# Loop over the entire B2 grid and get the psi,theta coordinates (and block number)
+import B2IO as b2
+b2dat = b2.B2("./test_data/b2_data/")
+R = b2dat.gmtry["crx"].mean(axis=2)
+Z = b2dat.gmtry["cry"].mean(axis=2)
 
+ptb = np.zeros((R.shape[0], R.shape[1], 3))
 
-#Here is how we would loop over the whole B2 grid to get all the data
-# but right now it is way too slow. Need to figure out a way to speed it up
-#import B2IO as b2
-#b2dat = b2.B2("./test_data/b2_data/")
-#R = b2dat.gmtry["crx"].mean(axis=2)
-#Z = b2dat.gmtry["cry"].mean(axis=2)
-#
-#ptb = np.zeros((R.shape[0], R.shape[1], 3))
-#
-#for i in range(R.shape[0]):
-#    for j in range(R.shape[1]):
-#        print(i,j)
-#        ptb[i,j] = gg.psitheta(R[i,j], Z[i,j])
+for i in range(R.shape[0]):
+    for j in range(R.shape[1]):
+        print(i,j)
+        ptb[i,j] = gg.psitheta(R[i,j], Z[i,j])
 
-
+np.save("./gkeyllGeometry/stored_data/gkeyll_b2_coordmapping.npy", ptb)
 
 
