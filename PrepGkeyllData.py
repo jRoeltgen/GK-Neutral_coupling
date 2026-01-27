d8d24b5b9fe308bcff55063efd62865be065acfd
Akash Shukla <akashukla@utexas.edu>
2026-01-27 15:30:54 -0800

    Add ionizing core boundary and correct fast reflection to use 65 degrees and all contributions to energy

--- PrepGkeyllData.py
+++ PrepGkeyllData.py
@@ -9,14 +9,15 @@ import eireneIO
 
 # First set data input paths
 gkeyll_data_path = './'
-gkeyll_simulation_name = 'hstep23'
+gkeyll_simulation_name = 'hstep26'
 gkeyll_half_domain=True
 gkeyll_diffusivity=0.5
+gkeyll_extra_species = ["molecule"]
 
-eirene_data_path = "./step_full_device_D_only/gkeyll_coupling/"
-b2_data_path = "./step_full_device_D_only/baserun/"
+eirene_data_path = "./step_full_device_D+D2_walled_off/gkeyll_w_2D2/"
+b2_data_path = "./step_full_device_D+D2_walled_off/baserun/"
 
-coordinate_mapping_path = './GK-Neutral_coupling/gkeyllGeometry/stored_data/'
+coordinate_mapping_path = './python_data/hstep26_geodata/'
 
 #Set data output paths and get frame number
 gkeyll_text_output_path = './gkeyll_text_output/'
@@ -24,7 +25,7 @@ eirene_output_path = './eirene_text_output/'
 frame = int(np.genfromtxt(gkeyll_text_output_path+"new_data_flag"))
 
 #Second load data
-g = gkeyllIO.gkeyll(gkeyll_data_path, gkeyll_simulation_name, gkeyll_half_domain, gkeyll_diffusivity)
+g = gkeyllIO.gkeyll(gkeyll_data_path, gkeyll_simulation_name, gkeyll_half_domain, gkeyll_diffusivity, gkeyll_extra_species)
 g.read_geometry()
 g.read_data(frame)
 g.read_coeffs(frame)
@@ -45,5 +46,5 @@ g.calc_derived_surfr_data(b2dat, edat)
 g.calc_derived_surfz_data(b2dat, edat)
 
 #4th populate and write eirene data
-g.populate_ft31(edat)
+g.populate_ft31(b2dat, edat)
 edat.write_ft31(eirene_data_path+'fort.31')
