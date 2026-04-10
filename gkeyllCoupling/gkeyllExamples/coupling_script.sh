ada8b3682bc9521e8141bab607a324caf77b984c
akashukla <akashukla@utexas.edu>
2026-04-10 10:44:01 -0500

    Change path to gkn library in coupling script example

--- gkeyllCoupling/gkeyllExamples/coupling_script.sh
+++ gkeyllCoupling/gkeyllExamples/coupling_script.sh
@@ -29,6 +29,7 @@
 
 RUNDIR=$(pwd)
 SOLPSRUNDIR=$(pwd)/<PATH TO YOUR SOLPS RUN>
+GKLIB_PATH=<PATH TO YOUR GKLIB INSTALL>
 
 module load python/3.12
 module load PrgEnv-gnu/8.6.0
@@ -67,7 +68,7 @@ while kill -0 $GKEYLL_PID 2> /dev/null; do
         echo "Detected new Gkeyll output frame ${frame}. Running post-processing..."
 
         # Run Python post-processing
-        python3 ./GK-Neutral_coupling/gkeyllIO/PrepGkeyllData.py
+        python3 ${GKLIB_PATH}/gkeyllIO/PrepGkeyllData.py
         
         echo "Done setting up EIRENE inputs"
 
@@ -87,7 +88,7 @@ while kill -0 $GKEYLL_PID 2> /dev/null; do
 
         echo "Done runnine EIRENE, Now converting eirene to Gkeyll input..."
 
-        python3 ./GK-Neutral_coupling/gkeyllIO/PrepEireneData.py
+        python3 .${GKLIB_PATH}/gkeyllIO/PrepEireneData.py
 
         # Remove flags to avoid re-processing
         rm -f gkeyll_text_output/new_data_flag 
