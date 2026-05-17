Fix a factor of sqrt 2 in the bflux correction, now it looks good. Fix some other if statements to run without D2+ in gkeyll

gkeyllCoupling/gkeyllIO/PrepGkeyllData.py
@@ -38,6 +38,7 @@ gkeyll_plate_material = gkeyll_options['plate_material']
 gkeyll_wall_material = gkeyll_options['wall_material']
 gkeyll_plate_rec_coeff = gkeyll_options['final_plate_rec_coeff']
 gkeyll_wall_rec_coeff = gkeyll_options['final_wall_rec_coeff']
+gkeyll_fast_reflection = gkeyll_options['fast_reflection']
 
 # Ensure extra_species is a list
 if not isinstance(gkeyll_extra_species, list):
@@ -52,7 +53,7 @@ gkeyll_text_output_path = paths['gkeyll_text_output_path']
 frame = int(np.genfromtxt(gkeyll_text_output_path+"new_data_flag"))
 
 #Second load data
-g = gkeyllIO.gkeyll(gkeyll_data_path, gkeyll_simulation_name, gkeyll_half_domain, gkeyll_diffusivity, gkeyll_extra_species, final_plate_rec_coeff = gkeyll_plate_rec_coeff, final_wall_rec_coeff=gkeyll_wall_rec_coeff, plate_material = gkeyll_plate_material, wall_material = gkeyll_wall_material)
+g = gkeyllIO.gkeyll(gkeyll_data_path, gkeyll_simulation_name, gkeyll_half_domain, gkeyll_diffusivity, gkeyll_extra_species, fast_reflection = gkeyll_fast_reflection, final_plate_rec_coeff = gkeyll_plate_rec_coeff, final_wall_rec_coeff=gkeyll_wall_rec_coeff, plate_material = gkeyll_plate_material, wall_material = gkeyll_wall_material)
 g.read_geometry()
 g.read_data(frame)
 g.read_coeffs(frame)
@@ -73,5 +74,5 @@ g.calc_derived_surfr_data(b2dat, edat)
 g.calc_derived_surfz_data(b2dat, edat)
 
 #4th populate and write eirene data
-g.populate_ft31(b2dat, edat)
+ft31 = g.populate_ft31(b2dat, edat)
 edat.write_ft31(eirene_data_path+'fort.31')

gkeyllCoupling/gkeyllIO/gkeyllIO.py
@@ -263,9 +263,9 @@ class gkeyll:
             #Correct the Flux based on bflux ratio
             for species in self.species_list:
                 if isim in [0,1]:
-                    mom_data[species+"M1"][:,0,:] = mom_data[species+"M1"][:,0,:]*np.abs(mom_data[species+"M1"+"ylower"][:,0]/mom_data[species+"M1"][:,0,0])[:, np.newaxis]
+                    mom_data[species+"M1"][:,0,:] = mom_data[species+"M1"][:,0,:]*np.abs( (mom_data[species+"M1"+"ylower"][:,0] * 1/np.sqrt(2) ) / (mom_data[species+"M1"][:,0,0] * 1/2 ) )[:, np.newaxis]
                 if isim in [4,5]:
-                    mom_data[species+"M1"][:,-1,:] = mom_data[species+"M1"][:,-1,:]*np.abs(mom_data[species+"M1"+"yupper"][:,0]/mom_data[species+"M1"][:,-1,0])[:, np.newaxis]
+                    mom_data[species+"M1"][:,-1,:] = mom_data[species+"M1"][:,-1,:]*np.abs( (mom_data[species+"M1"+"yupper"][:,0] * 1/np.sqrt(2) ) / (mom_data[species+"M1"][:,-1,0] * 1/2 ))[:, np.newaxis]
 
             # Load the potential
             mdata = pg.GData('%s-field_%d.gkyl'%(sim_name, frame))
@@ -876,6 +876,8 @@ class gkeyll:
         ft31["ti"] = self.interpolated_data["ti"]
         ft31["pr"] = self.interpolated_data["pr"]
         ft31["po"] = self.interpolated_data["po"]
+
+        return ft31
 
 
 